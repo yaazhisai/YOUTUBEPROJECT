422eace3e280ed31100e9365ea6aab785fd5c6e5
yaazhisai <yaazhisai@gmail.com>
2024-03-12 09:27:43 +0800

    Update you.py

--- you.py
+++ you.py
@@ -18,7 +18,7 @@ import time as t
 class project: 
     # def __init__(self):
     #connection
-    api_key='AIzaSyDV-c1x7Q3_izB_W-tDJWv4-szsE_BkOWw'
+    api_key=''
     api_service_name = "youtube"
     api_version = "v3"
     youtube = googleapiclient.discovery.build(
@@ -203,7 +203,7 @@ def create_dump(chn_out,filename):
          json.dump(chn_out, js)
 #creating database youtube1 ,in that creating collection named youtubedata and writing json data to MONGODB
 def update_mongo(doc_name):
-    uri = "mongodb+srv://yaazhisai:yaazhguvi@cluster0.d8lqkub.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://username:password@cluster0.d8lqkub.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
